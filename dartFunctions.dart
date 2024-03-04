f6f1b049732c059dc8a08869f914d84f0d82d819
Ridge <ridgemuturi@gmail.com>
2024-03-04 16:27:22 +0300

    Added dart functions

---
+++ dartFunctions.dart
@@ -0,0 +1,29 @@
+// Task 1: Write a function called addTwo that takes two numbers as arguments and returns the sum of those two numbers.
+num addTwo(num a, num b) {
+  return a + b;
+}
+
+// Task 2: Write a function called subtractTwo that takes two numbers as arguments and returns the difference of those two numbers.
+num subtractTwo(num a, num b) {
+  return a - b;
+}
+
+// Task 3: Write a function called multiplyTwo that takes two numbers as arguments and returns the product of those two numbers.
+num multiplyTwo(num a, num b) {
+  return a * b;
+}
+
+// Task 4: Write a function called divideTwo that takes two numbers as arguments and returns the quotient of those two numbers.
+num divideTwo(num a, num b) {
+  return a / b;
+}
+
+// Task 5: Write a function called stringLength that takes an argument of type String and returns the length of that string.
+int stringLength(String s) {
+  return s.length;
+}
+
+// Task 6: Write a function called getFirstElement that takes a list as an argument and returns the first element of that list.
+dynamic getFirstElement(List l) {
+  return l[0];
+}
