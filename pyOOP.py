d6944ca472b1f96f29b430a177e3afebb9f47cce
Ridge Benson <107855953+ridgebenson@users.noreply.github.com>
2024-03-31 19:14:11 +0300

    Added pyOOP.py

---
+++ pyOOP.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print(f"Hello! My name is {self.name}. I am {self.age} years old and identify as {self.gender}.")
+
+# Create an instance of the Person class
+person1 = Person(name="Ridge", age=21, gender="male")
+
+# Call the introduce method to display the person's information
+person1.introduce()
